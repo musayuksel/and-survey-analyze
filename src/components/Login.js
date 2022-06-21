1bbc0d1657888c72efacc2ffb6cfd441947d0816
sola-omoniyi <107858625+sola-omoniyi@users.noreply.github.com>
2022-06-21 16:27:47 +0100

    Added styling to login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,7 @@ import { signInWithGoogle } from "../firebase-config";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../App";
 import "../styles/login.css";
+import { BsPipFill } from "react-icons/bs";
 export default function Login() {
   const { user, setUser } = useContext(AuthContext);
   const navigate = useNavigate();
@@ -10,14 +11,24 @@ export default function Login() {
     navigate("/dashboard");
   }
   return (
-    <div>
-      Login PAGE!!!
+    <div className="login-page">
+
+        <img className="img" src="../Full Circle.png" alt="icon"/> 
+      
+      <h4 className="maintext">Log in to your Account</h4>
+      <br></br>
+      <h6 className="subtext">Sign in to access your onboarding feedback</h6>
+      <br></br>
+
+      <div className="space"></div>
+
       <button
         className="button-primary sign-in"
-        onClick={() => signInWithGoogle(setUser, navigate)}
-      >
-        Sign-in with Google
+        onClick={() => signInWithGoogle(setUser, navigate)}>
+        Continue with Google
       </button>
+
     </div>
+
   );
 }
