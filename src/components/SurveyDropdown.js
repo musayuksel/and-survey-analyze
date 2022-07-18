8de911b4fb4d35c96977573697d387335135a65c
Musa <79163385+musayuksel@users.noreply.github.com>
2022-07-18 23:14:47 +0100

    Add nanoid

--- src/components/SurveyDropdown.js
+++ src/components/SurveyDropdown.js
@@ -28,7 +28,6 @@ export default function SurveyDropdown({
   const printRef = useRef();
   const [showText, setShowText] = useState(false);
   const [currentPDF, setCurrentPDF] = useState();
-
   useEffect(() => {
     setCurrentPDF(
       eachQuestion.answerOptions.map((choice) => {
@@ -89,7 +88,7 @@ export default function SurveyDropdown({
     );
     return (
       <RepliesAnalyze
-        key={choice.id}
+        key={nanoid(5)}
         choiceText={choice.text}
         choiceId={choice.id}
         selectedAnswerCounter={selectedAnswerCounter}
